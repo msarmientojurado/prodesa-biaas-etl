8347cd018da4965cdc32b9f9fd829a0deb7f46e0
Miguel Sarmiento <msarmiento@procibernetica.com>
2021-09-20 11:07:53 -0500

    mdl_ar_mlstns_inicio_promesa.py coded to store the information in tbl_inicio_promesa at BigQuery.

--- libraries/model_area/milestones/mdl_ar_milestones.py
+++ libraries/model_area/milestones/mdl_ar_milestones.py
@@ -1,15 +1,18 @@
 
 
+from libraries.model_area.milestones.phases.mdl_ar_mlstns_inicio_promesa import mdl_ar_mlstns_inicio_promesa
 from libraries.model_area.milestones.phases.mdl_ar_mlstns_inicio_venta import mdl_ar_mlstns_inicio_venta
 
 
 def model_milestones(tbl_inicio_venta, tbl_inicio_promesa, tbl_inicio_construccion, tbl_inicio_escrituracion):
     #TODO
-    #   2. Operate over tbl_inicio_promesa
     #   4. Operate over tbl_inicio_escrituracion
 
     #Operate over tbl_inicio_venta
     mdl_ar_mlstns_inicio_venta(tbl_inicio_venta)
+
+    #Operate over tbl_inicio_promesa
+    mdl_ar_mlstns_inicio_promesa(tbl_inicio_promesa)
 
     #Operate over tbl_inicio_construccion
     #mdl_ar_mlstns_inicio_construccion(tmp_proyectos_construccion)
---
+++ libraries/model_area/milestones/phases/mdl_ar_mlstns_inicio_promesa.py
@@ -0,0 +1,46 @@
+
+
+from libraries.settings import TBL_INICIO_PROMESA
+from google.cloud import bigquery
+
+def mdl_ar_mlstns_inicio_promesa(tbl_inicio_promesa):
+    print("  *Model -tbl_inicio_promesa- Starting")
+    client = bigquery.Client()
+    # Since string columns use the "object" dtype, pass in a (partial) schema
+    # to ensure the correct BigQuery data type.
+    job_config = bigquery.LoadJobConfig(schema=[
+        bigquery.SchemaField("tip_regional",                    "STRING",   mode="NULLABLE"),
+        bigquery.SchemaField("tip_codigo_proyecto",             "STRING",   mode="NULLABLE"),
+        bigquery.SchemaField("tip_macroproyecto",               "STRING",   mode="NULLABLE"),
+        bigquery.SchemaField("tip_proyecto",                    "STRING",   mode="NULLABLE"),
+        bigquery.SchemaField("tip_etapa",                       "STRING",   mode="NULLABLE"),
+        bigquery.SchemaField("tip_dias_atraso",                 "INT64",    mode="NULLABLE"),
+        bigquery.SchemaField("tip_inicio_promesas_proyectado",  "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_inicio_promesas_programado",  "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_ent_kit_prom_proyectado",     "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_ent_kit_prom_programado",     "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_permiso_ventas_proyectado",   "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_permiso_ventas_programado",   "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_min_hipo_reg_proyectado",     "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_min_hipo_reg_programado",     "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_rad_min_hipo_reg_proyectado", "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_rad_min_hipo_reg_programado", "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_cred_construct_proyectado",   "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_cred_construct_programado",   "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_linderos_proyectado",         "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_linderos_programado",         "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_fai_proyectado",              "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_fai_programado",              "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_constitut_urban_proyectado",  "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_constitut_urban_programado",  "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_fecha_corte",                 "DATE",     mode="NULLABLE"),
+        bigquery.SchemaField("tip_fecha_proceso",               "DATETIME", mode="NULLABLE"),
+        bigquery.SchemaField("tip_lote_proceso",                "INT64",    mode="NULLABLE"),
+    ])
+
+    job = client.load_table_from_dataframe(
+        tbl_inicio_promesa, TBL_INICIO_PROMESA, job_config=job_config
+    )
+    # Wait for the load job to complete.
+    job.result()
+    print("  -Model -tbl_inicio_promesa- ending") 
