fe2c0667e9e8a8962151057ae7b32fa6841b76c9
Miguel Sarmiento <msarmiento@procibernetica.com>
2021-09-19 21:45:47 -0500

    bug fixed at mirror_area.py file

--- libraries/mirror_area/mirror_area.py
+++ libraries/mirror_area/mirror_area.py
@@ -1,4 +1,5 @@
 
+from libraries.settings_ import ORIGIN_FILE
 from libraries.settings import BLOB_NAME, BUCKET_NAME, ENVIRONMENT
 import pandas as pd
 from google.cloud import storage
@@ -8,7 +9,7 @@ def mirror_area(file_name):
     
     if ENVIRONMENT == "Development":
         #Uploading Consolidado
-        esp_consolidado_corte = pd.read_excel(file_name);
+        esp_consolidado_corte = pd.read_excel(ORIGIN_FILE);
     else: 
         #Taking the File from the Input Bucket
         storage_client = storage.Client()
