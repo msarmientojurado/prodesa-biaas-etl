d4a3c810e623f40960042f88290b9aba4673fb5b
Miguel Sarmiento <msarmiento@procibernetica.com>
2021-09-30 21:19:28 -0500

    validation mesh - checking missing project codes improvement

--- libraries/staging_area/information_consistency.py
+++ libraries/staging_area/information_consistency.py
@@ -68,10 +68,10 @@ def information_consistency(stg_consolidado_corte):
     #print(tbl_proyectos.head(5))
     project_codes=tbl_proyectos.tpr_codigo_proyecto.unique()
     tbl_proyectos=stg_consolidado_corte[~stg_consolidado_corte['stg_codigo_proyecto'].isin(project_codes)]
-    print("Codigos de Proyecto faltantes en la Tabla 'tbl_proyectos':")
-    #project_codes=tbl_proyectos.stg_codigo_proyecto.unique()
-    project_codes=[]
+    print("   Codigos de Proyecto faltantes en la Tabla 'tbl_proyectos':")
+    project_codes=tbl_proyectos.stg_codigo_proyecto.unique()
+    #project_codes=[]
     print(project_codes)
     if len(project_codes)>0:
-        print("Stop")
+        continue_process=False
     return stg_consolidado_corte, continue_process
